495e8a5339265af7cd4c15d310c60a444a15feda
Hans Huebner <hans.huebner@gmail.com>
2020-08-20 12:13:49 +0200

    Comment

--- set-dns-name-route53.sh
+++ set-dns-name-route53.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Set a domain name in Route53.  The complete domain name and the IPv4
+# address to associate with it need to be specified on the command
+# line.  AWS CLI needs to be configured so that the proper access key
+# is used, e.g. by the way of the default configuration, by setting
+# the AWS_PROFILE environment variable or by some other, equivalent
+# means
+# (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html).
+
 set -e
 
 DNSNAME=$1
